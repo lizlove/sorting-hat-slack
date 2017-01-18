e6819c203267ea74a885dfa2c1296623bddb0554
lizlove <elizabethlovero@gmail.com>
2017-01-18 15:57:29 -0500

    Add emojis to house messages.

--- houses.js
+++ houses.js
@@ -1,18 +1,18 @@
 module.exports = [
   {
     title: "gryffindor",
-    body: "You belong in *_Gryffindor_*, where dwell the brave at heart. Daring, nerve, and chivalry set Gryffindors apart."
+    body: "🦁 You belong in *_Gryffindor_*, where dwell the brave at heart. Daring, nerve, and chivalry set Gryffindors apart. 🦁"
   },
   {
     title: "hufflepuff",
-    body: "You belong in *_Hufflepuff_*, where they are just and loyal. The patient Hufflepuffs are true and unafraid of toil."
+    body: "🐼 You belong in *_Hufflepuff_*, where they are just and loyal. The patient Hufflepuffs are true and unafraid of toil.🐼"
   },
   {
     title: "ravenclaw",
-    body: "You belong in *_Ravenclaw_*; you have a ready mind. There those of wit and learning will always find their kind."
+    body: "🦅 You belong in *_Ravenclaw_*; you have a ready mind. There those of wit and learning will always find their kind. 🦅"
   },
   {
     title: "slytherin",
-    body: "You belong in *_Slytherin_*, where you'll make real friends. Those cunning folks use any means to achieve their ends."
+    body: "🐍 You belong in *_Slytherin_*, where you'll make real friends. Those cunning folks use any means to achieve their ends. 🐍"
   },
 ];

bb5310f20dba4510929d67398e0b24471a925562
lizlove <elizabethlovero@gmail.com>
2017-01-19 14:06:50 -0500

    change order and image url.

--- houses.js
+++ houses.js
@@ -2,21 +2,21 @@ module.exports = [
   {
     title: "🦁 Gryffindor",
     name: "Gryffindor",
-    body: "You belong in *_Gryffindor_*, where dwell the brave at heart. Daring, nerve, and chivalry set Gryffindors apart."
+    body: "You belong in Gryffindor, where dwell the brave at heart. Daring, nerve, and chivalry set Gryffindors apart."
   },
   {
     title: "🐼 Hufflepuff",
     name: "Hufflepuff",
-    body: "You belong in *_Hufflepuff_*, where they are just and loyal. The patient Hufflepuffs are true and unafraid of toil."
+    body: "You belong in Hufflepuff, where they are just and loyal. The patient Hufflepuffs are true and unafraid of toil."
   },
   {
     title: "🦅 Ravenclaw",
     name: "Ravenclaw",
-    body: "You belong in *_Ravenclaw_*; you have a ready mind. There those of wit and learning will always find their kind."
+    body: "You belong in Ravenclaw; you have a ready mind. There those of wit and learning will always find their kind."
   },
   {
     title: "🐍 Slytherin",
     name: "Slytherin",
-    body: "You belong in *_Slytherin_*, where you'll make real friends. Those cunning folks use any means to achieve their ends."
+    body: "You belong in Slytherin, where you'll make real friends. Those cunning folks use any means to achieve their ends."
   },
 ];
